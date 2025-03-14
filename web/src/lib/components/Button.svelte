c6b090cc0b4e37a2762dd7410f9fe0434328180f
Kapisolutions <biuro@kapisolutions.pl>
2025-03-14 13:55:15 +0100

    chore: update Button styles

--- web/src/lib/components/Button.svelte
+++ web/src/lib/components/Button.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-	import type { Snippet } from "svelte"
+	import type { Snippet } from 'svelte'
 
 	type Props = {
 		children: Snippet
 		onClick?: (e: Event) => void
-		type?: "button" | "submit" | "reset"
+		type?: 'button' | 'submit' | 'reset'
 		disabled?: boolean
 		loading?: boolean
+		white?: boolean
 	}
-	let { children, onClick, type = "button", disabled = false, loading = false }: Props = $props()
+	let {
+		children,
+		onClick,
+		type = 'button',
+		disabled = false,
+		loading = false,
+		white
+	}: Props = $props()
 </script>
 
-<button onclick={onClick} {type} disabled={disabled || loading} class:disabled={disabled || loading}
+<button
+	onclick={onClick}
+	{type}
+	disabled={disabled || loading}
+	class:disabled={disabled || loading}
+	class:white
 	>{#if loading}
 		<span class="spinner"></span>
 	{/if}{@render children()}</button
@@ -27,7 +40,9 @@
 		text-align: center;
 		text-decoration: none;
 		display: flex;
-		place-items: center;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
 		font-size: 16px;
 		cursor: pointer;
 	}
@@ -46,10 +61,14 @@
 		animation: spin 1s ease-in-out infinite;
 		-webkit-animation: spin 1s ease-in-out infinite;
 	}
+	.white {
+		background-color: var(--text-light);
+		color: var(--text-dark);
+	}
 
 	@keyframes spin {
 		to {
-			-webkit-transform: rotate(360deg);
+			transform: rotate(360deg);
 		}
 	}
 	@-webkit-keyframes spin {

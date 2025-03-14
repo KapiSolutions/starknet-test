ad8527e353947f80ab915b551eb5aacc6200cd37
Kapisolutions <biuro@kapisolutions.pl>
2025-03-14 14:19:59 +0100

    chore: add config

---
+++ web/src/config/config.ts
@@ -0,0 +1,11 @@
+export const SEPOLIA_CONTRACT_ADDRESS =
+	'0x0645af5648f7cb7a51c197c89bf8b1173c9bc17cdc62f26fcd2208daa675f884'
+export const DEVNET_CONTRACT_ADDRESS =
+	'0x039d73749cb23fad8016fa3197e43494edfec3e360e547877bdc4bdf04d13eff'
+
+export const DEVNET_ACCOUNT = {
+	address: '0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691',
+	privateKey: '0x71d7bb07b9a64f6f78ac4c816aff4da9',
+	cairo: undefined,
+	version: '0x3'
+}
